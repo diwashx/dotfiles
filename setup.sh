0888671d357fcd5a9a3fc5ee7cc86bf5ddef7cd8
diwashx <diwashx07@gmail.com>
2021-08-23 16:34:43 +0545

    wip

--- setup.sh
+++ setup.sh
@@ -6,4 +6,10 @@
 git clone https://github.com/diwashx/dotfiles.git
 
 
-sudo pacman -S bspwm sxhkd rofi nitrogen picom kitty
+sudo pacman -S bspwm sxhkd rofi nitrogen picom kitty neovim
+yay -S polybar
+mkdir ~/.config/bspwm
+mkdir ~/.config/sxhkd
+cp dotfiles/.config/bspwm/bspwmrc ~/.config/bspwm/
+cp dotfiles/.config/sxhkd/sxhkdrc ~/.config/sxhkd/
+
